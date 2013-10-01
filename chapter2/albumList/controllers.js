e7cfada94d9a5554cafb9edae3601a5c9d838579
Grisha Angelov <grisha.angelov89@abv.bg>
2013-10-01 21:18:48 +0300

    print index, name and duration of tracks in album

---
+++ chapter2/albumList/controllers.js
@@ -0,0 +1,11 @@
+var album =  [
+			{name:'Southwest Serenade', duration: '2:34'},
+            {name:'Northern Light Waltz', duration: '3:21'},
+            {name:'Eastern Tango', duration: '17:45'}
+		];
+		
+function albumController($scope){
+	
+	$scope.album = album;
+	
+}
