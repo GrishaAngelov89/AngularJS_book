583e6273ab4592770a8d3b99496ec6f98dc70884
Grisha Angelov <grisha.angelov89@abv.bg>
2013-10-01 20:35:35 +0300

    student list printed with ng-repeat

---
+++ chapter2/studentList/controllers.js
@@ -0,0 +1,14 @@
+var students = [
+		{name:'Mary Contrary',id:1},
+		{name:'Jack Sprat',id:2},
+		{name:'Jill Hill',id:3}
+	];
+
+function studentListController($scope){
+	
+	$scope.students = students;
+	
+	$scope.insertTom = function(){
+		$scope.students.splice(1,0,{name:'Tom Thumb',id:4});
+	}
+}
