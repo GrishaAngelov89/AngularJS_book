05c2c9384908f0731d42aa45466254101e0fb3f6
Grisha Angelov <grisha.angelov89@abv.bg>
2013-10-01 20:30:39 +0300

    startUpCalculator with reset button

---
+++ chapter2/startUpCalculator_reset/controllers.js
@@ -0,0 +1,19 @@
+
+function StartUpController($scope){
+	$scope.funding = {startingEstimate :0};
+	
+	$scope.computeNeeded = function(){
+	
+		$scope.funding.needed = $scope.funding.startingEstimate * 10;
+	};
+	
+	$scope.requestFunding = function(){
+		window.alert('Sorry, please get more customers first.');
+	};
+	
+	$scope.reset = function(){
+		$scope.funding.startingEstimate = 0;
+		$scope.funding.needed = 0;
+	};
+
+}
