6cced3c9e36e2a4ea2a0b32f1a484cb880f6a293
Grisha Angelov <grisha.angelov89@abv.bg>
2013-10-02 21:11:49 +0300

    toggle menu

---
+++ chapter2/hide_and_show/controllers.js
@@ -0,0 +1,11 @@
+
+
+function DeathrayMenuController($scope){
+
+	$scope.menuState = {show:false};
+	
+	$scope.toggleMenu = function (){
+	
+		$scope.menuState.show = !$scope.menuState.show;
+	};
+}
