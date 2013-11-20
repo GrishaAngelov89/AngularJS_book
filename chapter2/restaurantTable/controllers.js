a49db6dc0e9a854db288b9a18dc4f402498112b2
Grisha Angelov <grisha.angelov89@abv.bg>
2013-11-20 21:14:07 +0200

    Color selected table row

---
+++ chapter2/restaurantTable/controllers.js
@@ -0,0 +1,15 @@
+
+function RestaurantTableController($scope){
+
+	$scope.directory = [
+					  {name:'The Handsome Heifer', cuisine:'BBQ'},
+                      {name:'Green\'s Green Greens', cuisine:'Salads'},
+                      {name:'House of Fine Fish', cuisine:'Seafood'}
+	];
+	
+	$scope.selectRestaurant = function(row){
+		$scope.selectedRow = row;		
+	};
+	
+	
+}
